7320ec6b72758c3e7c9b3230743a4c36e2808b8d
spopovici <spopovici@promnetwork.com>
2018-05-30 11:05:19 -0400

    modified reading int from stdin

--- src/main/java/Permutation.java
+++ src/main/java/Permutation.java
@@ -2,7 +2,7 @@ import edu.princeton.cs.algs4.StdIn;
 
 public class Permutation {
     public static void main(String[] args) {
-        Integer k = StdIn.readInt();
+        Integer k = Integer.parseInt(args[0]);
         RandomizedQueue<String> rq = new RandomizedQueue<String>();
         for (int i = 0; i < k; i++) {
             rq.enqueue(StdIn.readString());
